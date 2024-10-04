cab2f5c16cf26c0c177bc08db01ec1512543150c
dantas <lucas01592@gmail.com>
2024-10-04 08:04:00 -0300

    atividade pool

--- poo_atividade/index.php
+++ poo_atividade/index.php
@@ -1,84 +1,80 @@
 <?php
 
-    class Chuteira
-    {
-        public $Modelo;
-        public $Valor;
 
-    }
+    class Chuteira 
+{
+    
+    public $marca;  
+    public $modelo; // Propriedades 
+    public $preco;    
 
-    $conta1 = new Chuteira();
-    $conta2 = new usuario();
-
-
-    echo '<pre>';
-    var_dump($conta1);
-    echo '<hr>';
-
-    echo '<pre>';
-    var_dump($conta2);
-    echo '<hr>';
-
-    class Produto
-    {
-        public $Modelo;
-        public $Valor;
-        public $Vendedor;
-
-
-        public function setModelo(Modelo $Modelo)
-        {
-            $this->Modelo = $Modelo;
-        }
-
-        public function getModelo()
-        {
-            return $this->modelo;
-        }
-
-        public function setValor(valor $Valor )
-        {
-            $this->valor = $Valor;
-        }
-
-        public function getValor()
-        {
-           return $this->valor;
-        }
-           
-    }
-
-    $objModelo = new Modelo();
-    $objModelo->nome = 'NIKE';
-
-    $objValor = new VAlor();
-    $objValor->setDados([
-        'valor' => 'R$ 600'
+    // Construtor da classe
+    public function chute($marca, $modelo,$preco) // os parametros 
+    {     
         
-    ]);
-
-
-    class usuario
-    {
-        public $Nome;
-        public $cpf;
+        $this->marca = $marca;  
+        $this->modelo = $modelo;    // propriedades 
+        $this->preco = $preco;     
     }
 
-    class Vendedor 
+    
+    public function exibirInformacoes() 
+    {
+        echo '<hr>';
+        echo "Marca : " . $this->marca . "<br>"; 
+        echo "Modelo : " . $this->modelo . "<br>";                  // Método para exibir as informações das chuteiras
+        echo "Valor R$: " . $this->preco . " Reais" . "<br><br>";
+        echo '<hr>';      
+    }
+}
+    
+    $chuteira1 = new Chuteira();    // objetos
+    $chuteira2 = new Chuteira();
+
+    $chuteira1->chute("NIKE", "Chuteira Mercurial Superfly","560.49");
+    $chuteira2->chute("ADIDAS", "Chuteira Adidas Predador","600.00");
+
+    
+    $chuteira1->exibirInformacoes();    // metodo para exibir as informações das chuteiras
+    $chuteira2->exibirInformacoes();
+
+
+    class Lojas
     {
         public $nome;
+        public $endereco;   //propiedades
         public $telefone;
+        public $horario;
 
-        public function getNome()
-        {
-            return $this->nome;
-        }
-
-        public function getTelefone()
-        {
-            return $this->telefone;
-        }
+    
+    public function __construct($nome,$endereco, $telefone, $horario) 
+    {
+        $this->nome = $nome;
+        $this->endereco = $endereco;
+        $this->telefone = $telefone;    //contrutor da classe loja
+        $this->horario = $horario;
     }
 
+    
+    public function exibirInformacoes() 
+    {
+        echo '<hr>';
+        echo "Nome da empresa: " . $this->nome . "<br>";
+        echo "Endereço: " . $this->endereco . "<br>";   // exibir as informações da empresa
+        echo "Telefone: " . $this->telefone . "<br>";
+        echo "Horario: " . $this->horario . "<br><br>";
+        echo '<hr>';
+    }
+
+    }
+
+       
+        $loja1 = new Lojas("NIKE  LOJA-OFICIAL", "Avenida Itaquera n:280", "(11) 70707-07070", "08:00-19:00");    // objeto da classe loja
+        $loja2 = new Lojas("ADIDAS BRASIL", "Avenida Corinthians Itaquera n:1910", "(11) 55966-07070", "08:00-19:00");
+
+        
+        $loja1->exibirInformacoes();    // método para exibir as informações da loja
+        $loja2->exibirInformacoes();
 
 ?>
+
