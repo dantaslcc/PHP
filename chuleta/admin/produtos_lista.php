43b57992746dae6f7c8bcf523ce4e7a3d061a8ca
dantas <lucas01592@gmail.com>
2024-10-11 11:48:04 -0300

    atividade 11

--- chuleta/admin/produtos_lista.php
+++ chuleta/admin/produtos_lista.php
@@ -2,7 +2,7 @@
 <?php
 include 'acesso_com.php';
 include '../conn/connect.php';
-$lista = $conn->query("select *from produtos");
+$lista = $conn->query("select *from vw_produtos");
 $row = $lista->fetch_assoc();
 $rows = $lista->num_rows;
 
